abe3ab8b1adc854ff3ca8026f5337ecd1a66000d
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2025-01-22 13:54:58 -0300

    chore: improve worker custom implementation sample (#57)

--- worker/tests/nbl-custom/README.md
+++ worker/tests/nbl-custom/README.md
@@ -1,2 +1,30 @@
-### Nbl Custom Worker
-A simple implementation for testing.
+## Nbl Custom Worker
+A simple implementation for testing. It shows how to define your own data structures to parse `config` and `scope` data. Also returns a `Device` entity to be ingested.
+
+
+## Policy samples
+
+### Sample list scope
+``` yaml
+policies:
+    custom_1:
+        config:
+          package: nbl_custom
+          custom: any_value
+        scope:
+          - hostname: 10.90.0.50
+            password: *****
+            username: admin
+```
+
+### Sample map scope
+``` yaml
+policies:
+    custom_1:
+        config:
+          package: nbl_custom
+          custom: any_value
+        scope:
+           target: 10.90.0.50
+           port: 8080
+```
--- worker/tests/nbl-custom/nbl_custom/impl.py
+++ worker/tests/nbl-custom/nbl_custom/impl.py
@@ -2,12 +2,39 @@
 # Copyright 2025 NetBox Labs Inc
 """NetBox Labs - Mock Impl."""
 
+import logging
 from collections.abc import Iterable
-from typing import Any
 
 from netboxlabs.diode.sdk.ingester import Device, Entity
+from pydantic import BaseModel, Field, ValidationError
 from worker.backend import Backend
 from worker.models import Metadata, Policy
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class CustomConfig(BaseModel):
+    """Validation model for config field."""
+
+    package: str = Field(..., description="Package name")
+    custom: str = Field(..., description="Custom field, can be of any type")
+
+
+class ScopeItem(BaseModel):
+    """Validation model for individual scope entries."""
+
+    hostname: str = Field(..., description="Target hostname or IP")
+    username: str = Field(..., description="Login username")
+    password: str = Field(..., description="Login password")
+
+
+class ScopeMap(BaseModel):
+    """Validation model for individual scope entries."""
+
+    target: str = Field(..., description="Target hostname or IP")
+    port: int = Field(..., description="Target port number")
 
 
 class MockBackend(Backend):
@@ -21,6 +48,13 @@ class MockBackend(Backend):
         """Mock run method."""
         entities = []
 
+        config = CustomConfig(**policy.config.model_dump())
+        # Sample of how to handle a scope list
+        if isinstance(policy.scope, list):
+            scope = [ScopeItem(**item) for item in policy.scope]
+        # Sample of how to handle a scope dictionary
+        elif isinstance(policy.scope, dict):
+            scope = ScopeMap(**policy.scope)
         device = Device(
             name="Device A",
             device_type="Device Type A",
@@ -35,6 +69,8 @@ class MockBackend(Backend):
             status="active",
             tags=["tag 1", "tag 2"],
         )
+        logger.info(f"Policy '{policy_name}' config: {config}")
+        logger.info(f"Policy '{policy_name}' scope: {scope}")
 
         entities.append(Entity(device=device))
         return entities
