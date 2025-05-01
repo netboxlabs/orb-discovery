17386fa042c0f9dad3476f2cf4fedf15c262c606
James Jeffries <jjeffries@netboxlabs.com>
2025-05-01 14:46:20 +0100

    corrects port

--- snmp-discovery/cmd/main.go
+++ snmp-discovery/cmd/main.go
@@ -39,7 +39,7 @@ func resolveEnv(value string) string {
 
 func main() {
 	host := flag.String("host", "0.0.0.0", "server host")
-	port := flag.Int("port", 8073, "server port")
+	port := flag.Int("port", 8070, "server port")
 	diodeTarget := flag.String("diode-target", "", "diode target (REQUIRED)")
 	diodeClientID := flag.String("diode-client-id", "", "diode client ID (REQUIRED)."+
 		" Environment variables can be used by wrapping them in ${} (e.g. ${MY_DIODE_CLIENT_ID})")
