31c9f6b9e980969233177e140f50565e0af02163
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2025-06-24 15:55:15 -0300

    feat: add dry run mode support for network-discovery

--- network-discovery/policy/runner_test.go
+++ network-discovery/policy/runner_test.go
@@ -26,6 +26,11 @@ func (m *MockClient) Ingest(ctx context.Context, entities []diode.Entity) (*diod
 	return args.Get(0).(*diodepb.IngestResponse), args.Error(1)
 }
 
+func (m *MockClient) IngestProto(ctx context.Context, entities []*diodepb.Entity) (*diodepb.IngestResponse, error) {
+	args := m.Called(ctx, entities)
+	return args.Get(0).(*diodepb.IngestResponse), args.Error(1)
+}
+
 func (m *MockClient) Close() error {
 	args := m.Called()
 	return args.Error(0)
