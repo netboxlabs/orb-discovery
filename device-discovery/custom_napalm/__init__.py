a918207c215838327484801f460c7bf2e903c02f
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2026-04-09 15:41:43 -0300

    feat: add HP ProCurve NAPALM driver (OBS-2599) (#315)

--- device-discovery/custom_napalm/__init__.py
+++ device-discovery/custom_napalm/__init__.py
@@ -2,8 +2,9 @@
 
 from custom_napalm.asa import ASADriver
 from custom_napalm.asa_ssh import ASASSHDriver
+from custom_napalm.hp_procurve import ProCurveDriver
 from custom_napalm.huawei_vrp import VRPDriver
 from custom_napalm.panos import PANOSDriver
 from custom_napalm.panos_ssh import PANOSSHDriver
 
-__all__ = ["ASADriver", "ASASSHDriver", "VRPDriver", "PANOSDriver", "PANOSSHDriver"]
+__all__ = ["ASADriver", "ASASSHDriver", "ProCurveDriver", "VRPDriver", "PANOSDriver", "PANOSSHDriver"]
