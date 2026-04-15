2b6a28afcd4f7d11abae58c5402b1f2a3f8f8ff4
Leo Parente <23251360+leoparente@users.noreply.github.com>
2026-04-15 08:49:30 -0300

    fix ordering

--- device-discovery/custom_napalm/__init__.py
+++ device-discovery/custom_napalm/__init__.py
@@ -11,12 +11,11 @@ from custom_napalm.fastiron import FastIronDriver
 from custom_napalm.ftd_ssh import FTDSSHDriver
 from custom_napalm.ftos import FTOSDriver
 from custom_napalm.gaia import GaiaDriver
-from custom_napalm.comware import ComwareDriver
-from custom_napalm.procurve import ProcurveDriver
 from custom_napalm.huawei_vrp import VRPDriver
 from custom_napalm.netiron import NetIronDriver
 from custom_napalm.panos import PANOSDriver
 from custom_napalm.panos_ssh import PANOSSHDriver
+from custom_napalm.procurve import ProcurveDriver
 from custom_napalm.ros import ROSDriver
 from custom_napalm.saos import SAOSDriver
 from custom_napalm.sros import SROSDriver
@@ -30,15 +29,14 @@ __all__ = [
     "AOSCXSSHDriver",
     "ASADriver",
     "ASASSHDriver",
+    "ComwareDriver",
     "ERSDriver",
     "ExosDriver",
     "FastIronDriver",
     "FTDSSHDriver",
     "FTOSDriver",
     "GaiaDriver",
-    "ComwareDriver",
     "NetIronDriver",
-    "ProcurveDriver",
     "ROSDriver",
     "SAOSDriver",
     "SROSDriver",
@@ -46,6 +44,7 @@ __all__ = [
     "VRPDriver",
     "PANOSDriver",
     "PANOSSHDriver",
+    "ProcurveDriver",
     "ViptelaSSHDriver",
     "VSPDriver",
     "WLCDriver",
